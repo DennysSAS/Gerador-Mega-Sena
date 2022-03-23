539eff0e0abd11e05d30351943db29c7b2b86402
DennysSAS <dennys.trab1@gmail.com>
2022-03-23 09:26:25 -0300

    testando

--- MegaSena.py
+++ MegaSena.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python3
-import random 
+import random
+from tkinter import N 
 
-jogos = int(input("Numero de jogos: "))
 
+jogos = int(input("Numero jogos: "))
+numero_jogado = input("Numero jogado: ")
 ## Função de geração de numeros randomicos
+def test():
+    quadrante = [range(0, 100)]
 
-def gerador():
-    quadrante = [[4, 11, 13, 23, 24], [6, 10, 30, 28, 16, 27, 17, 29], [32, 33, 34, 35, 41, 42, 43, 44, 51, 53, 54], [36, 38, 49, 56]]
     random_num1 = random.choice(quadrante[0])
-    random_num2 = random.choice(quadrante[1])
-    random_num3 = random.choice(quadrante[2])
-    random_num4 = random.choice(quadrante[3])
-    random_num5 = random.choice(quadrante[2])
-    random_num6 = random.choice(quadrante[1])
+    if random_num1 <= 9:
+        random_num1 = "{}{}".format(0,random_num1)
 
-    n_completo = ("{} {} {} {} {} {}".format(random_num1, random_num2, random_num3, random_num4, random_num5, random_num6))
-    print (n_completo)
+    random_num2 = random.choice(quadrante[0])
+    if random_num2 <= 9:
+        random_num2 = "{}{}".format(0,random_num2)
+
+    random_num3 = random.choice(quadrante[0])
+    if random_num3 <= 9:
+        random_num3 = "{}{}".format(0,random_num3)
+
+    random_num4 = random.choice(quadrante[0])
+    if random_num4 <= 9:
+        random_num4 = "{}{}".format(0,random_num4)
+
+    random_num5 = random.choice(quadrante[0])
+    if random_num5 <= 9:
+        random_num5 = "{}{}".format(0,random_num5)
+
+    random_num6 = random.choice(quadrante[0])
+    if random_num6 <= 9:
+        random_num6 = "{}{}".format(0,random_num6)
+
+    print ("|{}|{}|{}|{}|{}|{}|".format(random_num1, random_num2, random_num3, random_num4, random_num5, random_num6))
+
+
+## Validação
 
 ## Laço de repetição
+contador = 0
+while contador < jogos:
+    contador = contador + 1
+    test()
 
-def exec():
-   contador = 0 
-   while jogos > contador:
-       gerador()
-       contador = contador + 1        
 
-exec()
